e8827e6f1fdf79eccb161ed1ae34cbf2c0745a91
Artem Nizamiiev <upuyayua@gmail.com>
2025-12-19 14:15:18 +0200

    solved

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "matenizartem"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+
+    use_azuread_auth = true
+    use_oidc         = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
